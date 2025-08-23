98600de5751583b17dc47346b5732ee9c5163bd3
Minh Le <minhle@Mac.fritz.box>
2025-08-23 22:14:36 +0200

    Disable next-pwa; use custom SW

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import withPWA from 'next-pwa';
-
-const isProd = process.env.NODE_ENV === 'production';
-
-export default withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: !isProd,   // PWA off in dev, on in prod
-})({
+// next.config.mjs
+export default {
   reactStrictMode: true,
-});
+};
